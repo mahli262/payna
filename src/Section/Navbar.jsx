3871a3f9d1d5a30cbf2b2d8588957de2f8beaa97
ILHAM ILYASA <64563404+mahli262@users.noreply.github.com>
2024-10-28 18:42:13 +0700

    button navbar

--- src/Section/Navbar.jsx
+++ src/Section/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='hidden justify-between mx-[120px] max-w-full mt-[40px] max-h-[48px] items-center lg:flex' >
-        <a href="">
+        <button href="">
         <img src="../../assets/logo/logo.png" className='max-w-[150px]' alt="" />
-        </a>
+        </button>
         <ul className='flex space-x-[60px]'>
           <li className='hover:underline'>Home</li>
           <li className='hover:underline'>Features</li>
