609c26f27b4d20baf0f2f438e689a76f3343c4b1
ILHAM ILYASA <64563404+mahli262@users.noreply.github.com>
2024-10-28 17:56:40 +0700

    Brand section - fix

--- src/Section/Brand.jsx
+++ src/Section/Brand.jsx
@@ -6,12 +6,13 @@ function Brand() {
       <div>
         <p className='text-base font-semibold text-center lg:text-start mb-3 lg:mb-0'>Trusted by Global Companies</p>
       </div>
-      <div className='md:flex place-content-center md:gap-x-[50px] space-y-3  '>
-        <img src="/assets/logo/apple-11 1.png" className='md:max-h-[30px] max-w-[200px] w-full mx-auto max-h-[56px] my-auto' alt="" />
-        <img src="/assets/logo/google-2015 1.png" className='md:max-h-[30px] max-w-[200px] w-full mx-auto max-h-[56px]' alt="" />
-        <img src="/assets/logo/Group 13.png" className='md:max-h-[30px] max-w-[200px] w-full mx-auto max-h-[56px]' alt="" />
-        <img src="/assets/logo/slack-2 1.png" className='md:max-h-[30px] max-w-[200px] w-full mx-auto max-h-[56px]' alt="" />
-        <img src="/assets/logo/spotify-1 1.png" className='md:max-h-[30px] max-w-[200px] w-full mx-auto max-h-[56px]' alt="" />
+      {/* <div className='md:flex place-content-center md:gap-x-[50px] space-y-3  '> */}
+      <div className='mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5'>
+        <img src="/assets/logo/google-2015 1.png" className='col-span-2 max-h-12 w-full object-contain lg:col-span-1' alt="" />
+        <img src="/assets/logo/Group 13.png" className='col-span-2 max-h-12 w-full object-contain lg:col-span-1' alt="" />
+        <img src="/assets/logo/apple-11 1.png" className='col-span-2 max-h-12 w-full object-contain lg:col-span-1' alt="" />
+        <img src="/assets/logo/slack-2 1.png" className='col-span-2 max-h-12 w-full object-contain lg:col-span-1 sm:col-start-2' alt="" />
+        <img src="/assets/logo/spotify-1 1.png" className='col-span-2 max-h-12 w-full object-contain lg:col-span-1 sm:col-start-auto' alt="" />
       </div>
     </div>
   )
