8772d45f34555a2fece229f9cea4bf353f076e77
ILHAM ILYASA <64563404+mahli262@users.noreply.github.com>
2024-11-11 09:56:11 +0700

    footer - add source code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='text-center h-12'>
+    <div className='text-center h-12 flex justify-center gap-x-3'>
       <p>Design by <a href="https://shaynakit.com/landing">SHAYNAKIT</a></p>
+      <p> | </p>
+      <a href="https://github.com/mahli262/payna.git">Source Code</a>
     </div>
   )
 }
